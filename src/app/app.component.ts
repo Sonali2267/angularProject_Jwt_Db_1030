bd1224d44bb87acb2c948bda8043cda52c3e67ae
Sonali2267 <shamumehra988@gmail.com>
2023-08-26 11:27:18 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angularProject_Jwt_Db_1035';
+}
